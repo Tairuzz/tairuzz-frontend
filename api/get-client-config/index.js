a1830a1f5a6cb26647f517d356722dc11c65f08d
Tairuzz <sadiq@tairuzz.co.uk>
2026-08-15 18:27:52 +0100

    Add API endpoint for client configuration

---
+++ api/get-client-config/index.js
@@ -0,0 +1,28 @@
+module.exports = async function (req, res) {
+  res.json({
+    clientName: "Club A Analytics",
+    clientLogo: "/club-logo.png",
+    tabs: {
+      "League Table": true,
+      "Profile": true,
+      "Performance Rank": true,
+      "Goal Impact": true,
+      "Season on Season player": true,
+      "Season on Season Club": true,
+      "Fixture Results by xG": true,
+      "Team Results by xG": true,
+      "Title Race": true,
+      "Fixtures and Results": true,
+      "Attack Threat": true,
+      "KPIs Plot": true,
+      "Performance Metrics": true,
+      "Team Fixture Performance": true,
+      "Compare Performance Metrics": true,
+      "Goal Placement": true,
+      "Goal Categories": true,
+      "Goal Categories2": true,
+      "Goal Analysis": true,
+      "Goal Flow": true
+    }
+  });
+};
