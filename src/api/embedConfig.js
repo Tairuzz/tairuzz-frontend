7e9e84f52d22046d8213e571b3df39f54c5a5727
Tairuzz <sadiq@tairuzz.co.uk>
2026-09-09 19:39:42 +0100

    Update fetchEmbedConfig to use new API endpoint

--- src/api/embedConfig.js
+++ src/api/embedConfig.js
@@ -1,7 +1,11 @@
 export async function fetchEmbedConfig() {
-  const res = await fetch("/api/get-embed-config", {
-    headers: { "x-tairuzz-auth": localStorage.getItem("tairuzz_auth") }
-  });
+  const token = localStorage.getItem("tairuzz_auth");
+  const res = await fetch(
+    "https://tairuzz-backend-e2gjewe6f2fpewg8.ukwest-01.azurewebsites.net/api/embed-config",
+    {
+      headers: { Authorization: `Bearer ${token}` }
+    }
+  );
   if (!res.ok) throw new Error("Embed config failed");
   return res.json();
 }
